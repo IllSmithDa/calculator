ac660380963571095cb06f43c199db58d54ef3b7
Sam Kim <sams404link@gmail.com>
2018-03-21 09:41:33 -0700

    added buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -13,15 +13,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
       </div>
     );
   }
