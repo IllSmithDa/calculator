def6758c1ef22a4fc201dddba5d0c70b1e75dfb3
Sam Kim <sams404link@gmail.com>
2018-03-21 12:03:03 -0700

    fixed bugs

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -122,8 +122,11 @@ class App extends Component {
       if (this.state.inputValues[i] === '/') {
         currentValue = this.state.inputValues.splice(currentPostion, i)
         newValue = parseInt(currentValue.join(''))
-        otherValue = this.state.inputValues.slice(i + 1)
+        // console.log(newValue);
+        otherValue = this.state.inputValues.slice(i)
+        // console.log(otherValue);
         otherValue = parseInt(otherValue.join(''))
+        // console.log(otherValue);
         totalValue = newValue / otherValue;
         this.setState({currentInput: totalValue, inputValues: []})
         break;
